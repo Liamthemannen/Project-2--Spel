540755d7948b6fba446291cda2770d81174dfb6e
Liamthemannen <liam.bj@icloud.com>
2026-01-16 13:08:47 +0100

    Player_movement_med_animationsV1

--- Kod/Player.gd
+++ Kod/Player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 
-const Speed = 300.0
-const Acceleration = 2.0
+@export var Speed = 300.0
+@export var Acceleration = 2.0
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
 var input_vector: Vector2
+
 
 func get_input():
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -16,6 +19,23 @@ func _process(delta: float) -> void:
 	
 	velocity = lerp(velocity, PlayerInput * Speed, delta * Acceleration)
 	
+	_animation(input_vector)
+	
 	move_and_slide()
 	
+func _animation(input):
+	if input.x > 0:
+		animation.flip_h = false
+		animation.play("Höger")
+	elif input.x < 0:
+		animation.flip_h = true	
+		animation.play("Höger")
 	
+	if input.x == 0:
+		if input.y > 0:
+			animation.play("Neråt")
+		elif input.y < 0:
+			animation.play("Uppåt")
+			
+	if input == Vector2.ZERO:
+		animation.play("Stilla")
