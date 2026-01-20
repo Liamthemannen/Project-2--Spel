dfdf44cbb80c60a2cc53c660b8ef2ebeff77000d
Liamthemannen <liam.bj@icloud.com>
2026-01-20 12:58:30 +0100

    Settings_menu_ingen_felhantering

--- Kod/paus_menu.gd
+++ Kod/paus_menu.gd
@@ -1,8 +1,7 @@
 extends Control
 
 @onready var paus_menu: Control = $"."
-@onready var settings: Control = $"../Settings"
-
+@onready var settings_menu: Control = $"../Settings_menu"
 
 func _on_spela_pressed() -> void:
 	Engine.time_scale = 1 # Gör spelet till normal hastighet
@@ -11,7 +10,7 @@ func _on_spela_pressed() -> void:
 func _on_options_pressed() -> void:
 	# Stänger ner pausmenyn och öppnar inställningar
 	paus_menu.visible = false
-	settings.visible = true	
+	settings_menu.visible = true	
 
 
 func _on_avsluta_pressed() -> void:
