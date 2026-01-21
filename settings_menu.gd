2a81f7ee7948345c73dc96abf451e03a2d92eff4
Liamthemannen <liam.bj@icloud.com>
2026-01-21 09:59:09 +0100

    Fullscreen_knapp

--- settings_menu.gd
+++ settings_menu.gd
@@ -7,4 +7,11 @@ func _on_volume_value_changed(value: float) -> void:
 
 
 func _on_mute_knapp_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(0, toggled_on)
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_full_screen_control_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
