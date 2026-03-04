083967ceea971f37114f90b97cbe622a0479f50c
Penguinking2020 <97188851+lelleboy@users.noreply.github.com>
2026-03-04 10:24:37 +0100

    börjat på själva sköpandet

--- Kod/shop_fönster.gd
+++ Kod/shop_fönster.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
+var shotgun_script
 
+func _ready() -> void:
+	# Hitta shotgun-scriptet
+	shotgun_script = get_tree().root.get_node("Main/SpelKaraktär/Shotgun")
 
 var shop_fönster
 
@@ -7,4 +11,11 @@ func _on_close_pressed() -> void:
 	get_node("Anim").play("transout")
 	self.visible = false
 	get_tree().paused = false
-	
+
+func _on_fire_rate_pressed() -> void:
+	if shotgun_script:
+		# Minska timer-värdet (snabbare fire rate)
+		shotgun_script.timer.wait_time -= 0.1  # Minska med 0.1 sekunder
+		print("Fire rate uppgraderad! Ny tid: ", shotgun_script.timer.wait_time)
+	else:
+		print("Kunde inte hitta shotgun-scriptet!")
