930aec414a6705497ea2a2eac0d93746f419250b
Penguinking2020 <97188851+lelleboy@users.noreply.github.com>
2026-03-04 13:05:25 +0100

    mer features

--- Kod/shop_fönster.gd
+++ Kod/shop_fönster.gd
@@ -13,9 +13,7 @@ func _on_close_pressed() -> void:
 	get_tree().paused = false
 
 func _on_fire_rate_pressed() -> void:
-	if shotgun_script:
-		# Minska timer-värdet (snabbare fire rate)
-		shotgun_script.timer.wait_time -= 0.1  # Minska med 0.1 sekunder
-		print("Fire rate uppgraderad! Ny tid: ", shotgun_script.timer.wait_time)
-	else:
-		print("Kunde inte hitta shotgun-scriptet!")
+	shotgun_script.timer.wait_time -= 0.1 
+
+
+		
