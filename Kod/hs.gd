46b5e1de5715924e91f48a25c9c8d36ad87a4461
Liamthemannen <liam.bj@icloud.com>
2026-03-11 09:28:24 +0100

    Update hs.gd

--- Kod/hs.gd
+++ Kod/hs.gd
@@ -6,6 +6,6 @@ func _ready():
 	var leaderboard = Scoremanager.load_leaderboard()
 	for entry in leaderboard:
 		var label = Label.new()
-		label.text = entry["name"] + " - " + str(int(entry["kills"])) + " kills"
+		label.text = entry[1] + " - " + str(int(entry[0])) + " kills"
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		vbox.add_child(label)
