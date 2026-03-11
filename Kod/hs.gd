1b3f5160116c2518bb18dcfb16e795f0cc93f373
Liamthemannen <liam.bj@icloud.com>
2026-03-11 09:58:08 +0100

    reset_score

--- Kod/hs.gd
+++ Kod/hs.gd
@@ -9,3 +9,8 @@ func _ready():
 		label.text = entry["name"] + " - " + str(int(entry["kills"])) + " kills"
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		vbox.add_child(label)
+
+
+func _on_button_pressed() -> void:
+	if FileAccess.file_exists("user://leaderboard.json"):
+		DirAccess.remove_absolute("user://leaderboard.json")
