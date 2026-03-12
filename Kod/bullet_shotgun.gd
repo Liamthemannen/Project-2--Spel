8e9670b6ed42b537d0dd0a5041768ce788a85880
Liamthemannen <liam.bj@icloud.com>
2026-03-12 13:44:36 +0100

    kommentera_shotgun

--- Kod/bullet_shotgun.gd
+++ Kod/bullet_shotgun.gd
@@ -8,16 +8,22 @@ var killed_amount = 0
 
 var travelled_distance = 0
 
+# Körs varje frame, gör så skoten rör sig
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
 	
+	# Håller koll på hur långt projektilen har färdats
 	travelled_distance += SPEED * delta
+	
+	# Ta bort skottet om den gått längre än tillåtet
 	if travelled_distance > RANGE:
-		queue_free()
+		queue_free() 
 
-
+# Körs automatiskt när projektilen kolliderar med en kropp
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	queue_free() # Tar bort skottet 
+	
+	# Om objektet kan ta skada av shotgun, så tar den skada
 	if body.has_method("take_damage_shotgun"):
 		body.take_damage_shotgun()
