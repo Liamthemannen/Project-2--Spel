6b031c31bb6b89fbfedd86fff56bb5e020725b29
Liamthemannen <liam.bj@icloud.com>
2026-03-13 16:30:20 +0100

    main_m kom

--- Kod/main_menu.gd
+++ Kod/main_menu.gd
@@ -2,11 +2,11 @@ extends Control
 @onready var line_edit: LineEdit = $LineEdit
 @onready var label: Label = $LineEdit/Label
 
-
+# Stänger av spelet
 func _on_button_3_pressed() -> void:
 	get_tree().quit()
 
-
+# Startar spelet
 func _on_button_2_pressed() -> void:
-	Scoremanager.player_name = line_edit.text
+	Scoremanager.player_name = line_edit.text # Sparar användares namn
 	get_tree().change_scene_to_file("res://Scen/main.tscn")
