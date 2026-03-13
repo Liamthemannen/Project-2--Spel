311c2f4a6d7353e75c7eecb47f0eedf5ae176204
Liamthemannen <liam.bj@icloud.com>
2026-03-13 16:28:02 +0100

    label kom

--- Kod/label.gd
+++ Kod/label.gd
@@ -2,4 +2,4 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.text = str(Scoremanager.killed_amount)
+	self.text = str(Scoremanager.killed_amount) # Visar antal kills
